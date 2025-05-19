126a0fdc99bf6e36da910c8cde12d02c3d58a5fd
Pedro Schwarz Rodrigues <pedroschawarz97@gmail.com>
2025-05-19 22:26:03 +1000

    Improve invites

--- lib/invites/invites.dart
+++ lib/invites/invites.dart
@@ -3,5 +3,5 @@ export './data/models/create_invite_request.dart';
 export './data/models/invite_response.dart';
 export './data/repository/invites_repository.dart';
 export './ui/cubit/invites_cubit.dart';
-export './ui/view/invite_users_sheet.dart';
-export './ui/view/invites_screen.dart';
+export 'ui/view/screens/invites_screen.dart';
+export 'ui/view/sheets/invite_users_sheet.dart';
---
+++ lib/invites/ui/view/screens/invites_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:task_master/app/app.dart';
+import 'package:task_master/invites/invites.dart';
+import 'package:task_master/invites/ui/view/components/invite_item.dart';
+
+class InvitesScreen extends StatefulWidget {
+  const InvitesScreen({super.key});
+
+  static const String routeName = 'invites';
+
+  @override
+  State<InvitesScreen> createState() => _InvitesScreenState();
+}
+
+class _InvitesScreenState extends State<InvitesScreen> {
+  final bloc = getIt<InvitesCubit>();
+
+  @override
+  void initState() {
+    super.initState();
+    bloc.load();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: NestedScrollView(
+        headerSliverBuilder:
+            (_, __) => [
+              SliverAppBar.medium(
+                title: const Text('Invites'),
+                bottom: PreferredSize(
+                  preferredSize: const Size(0, AppSpacing.s),
+                  child: BlocSelector<InvitesCubit, InvitesState, bool>(
+                    bloc: bloc,
+                    selector: (state) => state.isLoading,
+                    builder: (context, isLoading) => isLoading ? const LinearProgressIndicator() : const SizedBox.shrink(),
+                  ),
+                ),
+              ),
+            ],
+        body: BlocSelector<InvitesCubit, InvitesState, List<InviteResponse>>(
+          bloc: bloc,
+          selector: (state) => state.invites,
+          builder:
+              (context, invites) =>
+                  invites.isEmpty
+                      ? const Center(child: Text('No invitations for now.'))
+                      : ListView.separated(
+                        padding: const EdgeInsets.only(top: AppSpacing.s, bottom: AppSpacing.max),
+                        itemCount: invites.length,
+                        itemBuilder: (context, position) {
+                          return InviteItem(invite: invites[position], onAccepted: bloc.acceptInvite, onRejected: bloc.rejectInvite);
+                        },
+                        separatorBuilder: (_, __) => const Divider(),
+                      ),
+        ),
+      ),
+    );
+  }
+}
