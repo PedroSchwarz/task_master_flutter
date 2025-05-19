e78b4868816979ad49e0cba2cd0f943857340436
Pedro Schwarz Rodrigues <pedroschawarz97@gmail.com>
2025-05-20 03:42:11 +1000

    Update theme

--- lib/app/design_system/theme/app_theme.dart
+++ lib/app/design_system/theme/app_theme.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:task_master/app/design_system/theme/app_spacing.dart';
 import 'package:task_master/app/design_system/theme/app_typography.dart';
 
 class AppTheme {
-  static const Color primaryRed = Color(0xFFB71C1C);
-  static const Color secondaryColor = Color(0xFFE57373);
+  static const Color primaryPurple = Color(0xFF6750A4);
+  static const Color primaryPurpleLight = Color.fromARGB(255, 204, 189, 244);
+  static const Color secondaryColor = Color(0xFF625B71);
+
   static const Color lightBackgroundColor = Color(0xFFFDFDFD);
   static const Color lightInputBackgroundColor = Color.fromARGB(255, 225, 225, 225);
   static const Color darkBackgroundColor = Color.fromARGB(255, 46, 46, 46);
@@ -13,26 +14,21 @@ class AppTheme {
   static final ThemeData lightTheme = ThemeData(
     useMaterial3: true,
     brightness: Brightness.light,
-    primaryColor: primaryRed,
-    colorScheme: ColorScheme.fromSeed(
-      seedColor: primaryRed,
-      brightness: Brightness.light,
-      primary: primaryRed,
-      onPrimary: lightBackgroundColor,
-      secondary: secondaryColor,
-    ),
+    colorScheme: ColorScheme.fromSeed(seedColor: primaryPurple, brightness: Brightness.light),
     scaffoldBackgroundColor: lightBackgroundColor,
     textTheme: AppTypography.defaultTextTheme,
     appBarTheme: AppBarTheme(
       backgroundColor: lightBackgroundColor,
       foregroundColor: darkBackgroundColor,
-      titleTextStyle: AppTypography.defaultTextTheme.displaySmall?.copyWith(color: primaryRed),
+      titleTextStyle: AppTypography.defaultTextTheme.displaySmall?.copyWith(color: primaryPurple),
       centerTitle: false,
     ),
     inputDecorationTheme: InputDecorationTheme(
-      border: OutlineInputBorder(borderRadius: BorderRadius.circular(AppSpacing.xs)),
       filled: true,
       fillColor: lightInputBackgroundColor,
+      border: OutlineInputBorder(borderRadius: BorderRadius.circular(12), borderSide: BorderSide.none),
+      focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(12), borderSide: const BorderSide(color: primaryPurple, width: 2)),
+      enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(12), borderSide: BorderSide.none),
     ),
     filledButtonTheme: FilledButtonThemeData(style: FilledButton.styleFrom(textStyle: AppTypography.defaultTextTheme.titleMedium)),
     progressIndicatorTheme: ProgressIndicatorThemeData(trackGap: 3, strokeCap: StrokeCap.round, circularTrackColor: Colors.grey.shade300),
@@ -41,28 +37,21 @@ class AppTheme {
   static final ThemeData darkTheme = ThemeData(
     useMaterial3: true,
     brightness: Brightness.dark,
-    colorScheme: ColorScheme.fromSeed(
-      seedColor: primaryRed,
-      brightness: Brightness.dark,
-      primary: primaryRed,
-      onPrimary: lightBackgroundColor,
-      secondary: secondaryColor,
-    ),
+    colorScheme: ColorScheme.fromSeed(seedColor: primaryPurpleLight, brightness: Brightness.dark),
     scaffoldBackgroundColor: darkBackgroundColor,
     appBarTheme: AppBarTheme(
       backgroundColor: darkBackgroundColor,
       foregroundColor: lightBackgroundColor,
-      titleTextStyle: AppTypography.defaultTextTheme.displaySmall?.copyWith(color: primaryRed),
+      titleTextStyle: AppTypography.defaultTextTheme.displaySmall?.copyWith(color: primaryPurpleLight),
       centerTitle: false,
     ),
     textTheme: AppTypography.defaultTextTheme,
     inputDecorationTheme: InputDecorationTheme(
-      border: OutlineInputBorder(
-        borderSide: const BorderSide(color: lightBackgroundColor, width: 2),
-        borderRadius: BorderRadius.circular(AppSpacing.xs),
-      ),
       filled: true,
       fillColor: darkInputBackgroundColor,
+      border: OutlineInputBorder(borderRadius: BorderRadius.circular(12), borderSide: BorderSide.none),
+      focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(12), borderSide: const BorderSide(color: primaryPurpleLight, width: 2)),
+      enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(12), borderSide: BorderSide.none),
     ),
     filledButtonTheme: FilledButtonThemeData(style: FilledButton.styleFrom(textStyle: AppTypography.defaultTextTheme.titleMedium)),
     progressIndicatorTheme: ProgressIndicatorThemeData(trackGap: 3, strokeCap: StrokeCap.round, circularTrackColor: Colors.grey.shade300),
